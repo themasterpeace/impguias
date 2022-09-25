1e0fba11958abda98d5ea1de5e9318cc0be5b539
masterpeace <mesquite275@gmail.com>
2022-09-24 22:30:48 -0600

    terminando reporte por codigo exitosamente

--- guiasenv/views.py
+++ guiasenv/views.py
@@ -13,6 +13,8 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from django.utils.decorators import method_decorator
 from openpyxl import Workbook
+from openpyxl.styles import Alignment,Border,Font,PatternFill,Side
+
 from django.http.response import HttpResponse, JsonResponse
 
 from bases.views import SinPrivilegios
@@ -101,35 +103,110 @@ def entregado(request, id):
 
 class ReporteClienteExcel(TemplateView):
     def get(self, request, *args, **kwargs):
-        clientes = GuiasEnv.objects.all()
+        campo = request.GET.get('campo')
+        
+        clientes = GuiasEnv.objects.filter(codigo = campo)
+        
         wb = Workbook()
         ws = wb.active
-        ws['B1'] = 'REPORTE DE CLIENTES'
 
-        ws.merge_cells('B1:E1')
+        #Crear el título en la hoja
+        ws['A1'].alignment = Alignment(horizontal = "center",vertical = "center")
+        ws['A1'].border = Border(left = Side(border_style = "thin"), right = Side(border_style = "thin"),
+                                    top = Side(border_style = "thin"), bottom = Side(border_style = "thin") ) 
+        ws['A1'].fill = PatternFill(start_color = '66FFCC', end_color = '66FFCC', fill_type = "solid")
+        ws['A1'].font = Font(name = 'Calibri', size = 12, bold = True)
+        ws['A1'] = 'REPORTE DE CLIENTES'
 
-        ws['B3'] = 'FECHA'
-        ws['C3'] = 'CODIGO'
-        ws['D3'] = 'CLIENTE'
-        ws['E3'] = 'TIPO ENVIO'
-        ws['F3'] = 'NUMERO INICIAL'
-        ws['G3'] = 'NUMERO FINAL'
-        ws['H3'] = 'TOTAL ENVIADO'
-        ws['I3'] = 'FORMA PAGO'
-        ws['J3'] = 'ENTREGADO'
+        ws.merge_cells('A1:I1')
+        ws.row_dimensions[1].height = 25
 
-        cont = 9 
+        ws.column_dimensions['A'].width = 20
+        ws.column_dimensions['B'].width = 20
+        ws.column_dimensions['C'].width = 20
+        ws.column_dimensions['D'].width = 20
+        ws.column_dimensions['E'].width = 20
+        ws.column_dimensions['F'].width = 20
+        ws.column_dimensions['G'].width = 20
+        ws.column_dimensions['H'].width = 20
+        ws.column_dimensions['I'].width = 20
+
+
+        ws['A3'].alignment = Alignment(horizontal = "center", vertical = "center")
+        ws['A3'].border = Border(left = Side(border_style = "thin"), right = Side(border_style = "thin"),
+                                    top = Side(border_style = "thin"), bottom = Side(border_style = "thin") )
+        ws['A3'].fill = PatternFill(start_color = '66CFCC', end_color = '66CFCC', fill_type = "solid")
+        ws['A3'].font = Font(name = 'Calibro', size = 10, bold = True)
+        ws['A3'] = 'FECHA'
+
+        ws['B3'].alignment = Alignment(horizontal = "center", vertical = "center")
+        ws['B3'].border = Border(left = Side(border_style = "thin"), right = Side(border_style = "thin"),
+                                    top = Side(border_style = "thin"), bottom = Side(border_style = "thin") )
+        ws['B3'].fill = PatternFill(start_color = '66CFCC', end_color = '66CFCC', fill_type = "solid")
+        ws['B3'].font = Font(name = 'Calibro', size = 10, bold = True)
+        ws['B3'] = 'CODIGO'
+        
+        ws['C3'].alignment = Alignment(horizontal = "center", vertical = "center")
+        ws['C3'].border = Border(left = Side(border_style = "thin"), right = Side(border_style = "thin"),
+                                    top = Side(border_style = "thin"), bottom = Side(border_style = "thin") )
+        ws['C3'].fill = PatternFill(start_color = '66CFCC', end_color = '66CFCC', fill_type = "solid")
+        ws['C3'].font = Font(name = 'Calibro', size = 10, bold = True)
+        ws['C3'] = 'CLIENTE'
+
+        ws['D3'].alignment = Alignment(horizontal = "center", vertical = "center")
+        ws['D3'].border = Border(left = Side(border_style = "thin"), right = Side(border_style = "thin"),
+                                   top = Side(border_style = "thin"), bottom = Side(border_style = "thin") )
+        ws['D3'].fill = PatternFill(start_color = '66CFCC', end_color = '66CFCC', fill_type = "solid")
+        ws['D3'].font = Font(name = 'Calibro', size = 10, bold = True)
+        ws['D3'] = 'TIPO ENVIO'
+
+        ws['E3'].alignment = Alignment(horizontal = "center", vertical = "center")
+        ws['E3'].border = Border(left = Side(border_style = "thin"), right = Side(border_style = "thin"),
+                                    top = Side(border_style = "thin"), bottom = Side(border_style = "thin") )
+        ws['E3'].fill = PatternFill(start_color = '66CFCC', end_color = '66CFCC', fill_type = "solid")
+        ws['E3'].font = Font(name = 'Calibro', size = 10, bold = True)
+        ws['E3'] = 'NUMERO INICIAL'
+
+        ws['F3'].alignment = Alignment(horizontal = "center", vertical = "center")
+        ws['F3'].border = Border(left = Side(border_style = "thin"), right = Side(border_style = "thin"),
+                                    top = Side(border_style = "thin"), bottom = Side(border_style = "thin") )
+        ws['F3'].fill = PatternFill(start_color = '66CFCC', end_color = '66CFCC', fill_type = "solid")
+        ws['F3'].font = Font(name = 'Calibro', size = 10, bold = True)
+        ws['F3'] = 'NUMERO FINAL'
+
+        ws['G3'].alignment = Alignment(horizontal = "center", vertical = "center")
+        ws['G3'].border = Border(left = Side(border_style = "thin"), right = Side(border_style = "thin"),
+                                    top = Side(border_style = "thin"), bottom = Side(border_style = "thin") )
+        ws['G3'].fill = PatternFill(start_color = '66CFCC', end_color = '66CFCC', fill_type = "solid")
+        ws['G3'].font = Font(name = 'Calibro', size = 10, bold = True)
+        ws['G3'] = 'TOTAL ENVIADO'
+
+        ws['H3'].alignment = Alignment(horizontal = "center", vertical = "center")
+        ws['H3'].border = Border(left = Side(border_style = "thin"), right = Side(border_style = "thin"),
+                                    top = Side(border_style = "thin"), bottom = Side(border_style = "thin") )
+        ws['H3'].fill = PatternFill(start_color = '66CFCC', end_color = '66CFCC', fill_type = "solid")
+        ws['H3'].font = Font(name = 'Calibro', size = 10, bold = True)
+        ws['H3'] = 'FORMA PAGO'
+
+        ws['I3'].alignment = Alignment(horizontal = "center", vertical = "center")
+        ws['I3'].border = Border(left = Side(border_style = "thin"), right = Side(border_style = "thin"),
+                                    top = Side(border_style = "thin"), bottom = Side(border_style = "thin") )
+        ws['I3'].fill = PatternFill(start_color = '66CFCC', end_color = '66CFCC', fill_type = "solid")
+        ws['I3'].font = Font(name = 'Calibro', size = 10, bold = True)
+        ws['I3'] = 'ENTREGADO'
+
+        cont = 5 
 
         for cliente in clientes:
-            ws.cell(row = cont, column=2).value = cliente.fecha
-            ws.cell(row = cont, column=3).value = cliente.codigo
-            ws.cell(row = cont, column=4).value = cliente.cliente
-            ws.cell(row = cont, column=5).value = cliente.tipo_envio
-            ws.cell(row = cont, column=6).value = cliente.numini
-            ws.cell(row = cont, column=7).value = cliente.numfin
-            ws.cell(row = cont, column=8).value = cliente.totenvio
-            ws.cell(row = cont, column=9).value = cliente.fpago
-            ws.cell(row = cont, column=10).value = cliente.entregado
+            ws.cell(row = cont, column=1).value = cliente.fecha
+            ws.cell(row = cont, column=2).value = cliente.codigo
+            ws.cell(row = cont, column=3).value = cliente.cliente
+            ws.cell(row = cont, column=4).value = cliente.tipo_envio
+            ws.cell(row = cont, column=5).value = cliente.numini
+            ws.cell(row = cont, column=6).value = cliente.numfin
+            ws.cell(row = cont, column=7).value = cliente.totenvio
+            ws.cell(row = cont, column=8).value = cliente.fpago
+            ws.cell(row = cont, column=9).value = cliente.entregado
             cont+=1
 
         nombre_archivo = "GuiasImpresas.xlsx"
