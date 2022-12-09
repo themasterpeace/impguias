8dddc9c2a1cb2ac8bf19e1819a1bbdfb1a61d11b
masterpeace <mesquite275@gmail.com>
2022-12-08 22:53:54 -0600

    agregando highcharts al home pero sin exito

--- bases/views.py
+++ bases/views.py
@@ -2,7 +2,10 @@ import imp
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect, JsonResponse
 from django.urls import reverse_lazy
-from datetime import date
+from datetime import date, datetime
+from django.db.models.functions import Coalesce
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from collections import Counter
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views import generic
@@ -38,9 +41,37 @@ class Home(LoginRequiredMixin, generic.TemplateView):
     template_name = 'bases/home.html'
     login_url='bases:login'
 
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {}
+        try:
+            action = request.POST['action']
+            if action == 'get_report_sales_months':
+                data = self.get_report_sales_months()
+            else:
+                data['error'] = 'Ha ocurrido un error'
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data, safe=False)
+
+    def get_report_sales_months(self):
+        data = []
+        try:
+            year = datetime.now().year
+            for m in range(1, 13):
+                total = GuiasEnv.objects.filter(fecha_año=year, fecha_mes=m)
+                data.append(float(total))
+        except:
+            pass
+        return data
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["qs"]=GuiasEnv.objects.all()
+        context['panel']= 'Panel de Administracion'
+        context['report_sales_months'] = self.get_report_sales_months()
         return context
   
 
